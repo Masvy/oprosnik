c2a723ca9fed5e5ad0a261ce793f2abc0ea443af
Masvy <masvy.435@gmail.com>
2023-08-21 21:50:37 +0300

    feat: create a func. to initial. and start the bot

---
+++ tgbot/bot.py
@@ -0,0 +1,41 @@
+import logging
+import asyncio
+
+from environs import Env
+import betterlogging as bl
+from aiogram import Bot, Dispatcher
+
+
+def setup_logging():
+    log_level = logging.INFO
+    bl.basic_colorized_config(level=log_level)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s'
+               ' [%(asctime)s] - %(name)s - %(message)s',
+    )
+    logger = logging.getLogger(__name__)
+    logger.info('Starting bot')
+
+
+async def main():
+    setup_logging()
+
+    env = Env()
+    env.read_env()
+
+    bot: Bot = Bot(token=env('BOT_TOKEN'),
+                   parse_mode='HTML')
+    dp: Dispatcher = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error('Stopping bot')
