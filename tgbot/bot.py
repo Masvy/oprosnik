9bc00c19260b31ac8035af73302d646e2f6d417e
Masvy <masvy.435@gmail.com>
2023-08-22 12:29:46 +0300

    feat: add some important changes
    establish the connect. and create the table
    
    register router
    
    register middleware

--- tgbot/bot.py
+++ tgbot/bot.py
@@ -4,6 +4,13 @@ import asyncio
 from environs import Env
 import betterlogging as bl
 from aiogram import Bot, Dispatcher
+from sqlalchemy import URL
+
+from handlers import admin
+from database.create_table import BaseModel
+from database.engine import proceed_schemas
+from database.engine import create_async_engine, get_session_maker
+from middlewares.register_check import RegisterCheck
 
 
 def setup_logging():
@@ -29,9 +36,28 @@ async def main():
                    parse_mode='HTML')
     dp: Dispatcher = Dispatcher()
 
+    dp.message.middleware(RegisterCheck())
+    dp.callback_query.middleware(RegisterCheck())
+
+    dp.include_router(admin.admin_router)
+
+    postgres_url = URL.create(
+        'postgresql+asyncpg',
+        username=env('PGUSER'),
+        password=env('PGPASSWORD'),
+        database=env('DB_NAME'),
+        host='localhost',
+        port='5432'
+    )
+
+    async_engine = create_async_engine(postgres_url)
+    session_maker = get_session_maker(async_engine)
+
+    await proceed_schemas(async_engine, BaseModel.metadata)
+
     await bot.delete_webhook(drop_pending_updates=True)
 
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, session_maker=session_maker)
 
 
 if __name__ == '__main__':
