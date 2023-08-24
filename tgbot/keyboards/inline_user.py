cb7e5a3c3417700aa81f31e186657961f9bae1cd
Masvy <masvy.435@gmail.com>
2023-08-24 18:42:28 +0300

    chore: create a lot of keyboards objects
    create inline_user.py
    
    in inline_user.py create keyboards objects

---
+++ tgbot/keyboards/inline_user.py
@@ -0,0 +1,153 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# Создал объект клавиатуры
+question1: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question1_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question1_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question1_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question1_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question2: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question2_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question2_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question2_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question2_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question3: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question3_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question3_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question3_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question3_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question4: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question4_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question4_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question4_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question4_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question5: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question5_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question5_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question6: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question6_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question6_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question6_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question6_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question7: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question7_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question7_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question7_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question7_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question8: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question8_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question8_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question8_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question8_pressed1')
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question9: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question9_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question9_pressed1'),
+        ]
+    ]
+)
+
+# Создал объект клавиатуры
+question10: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='1',
+                                 callback_data='question10_pressed1'),
+            InlineKeyboardButton(text='2',
+                                 callback_data='question10_pressed1'),
+            InlineKeyboardButton(text='3',
+                                 callback_data='question10_pressed1'),
+            InlineKeyboardButton(text='4',
+                                 callback_data='question10_pressed1')
+        ]
+    ]
+)
