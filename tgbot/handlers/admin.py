d7a47743a796a8f3d72c5febced572913a820074
Masvy <masvy.435@gmail.com>
2023-08-24 18:32:41 +0300

    feat: create a send function
    in admin.py create two handlers for mailin list

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,10 +1,14 @@
-from aiogram import Router
-from aiogram.types import Message
-from aiogram.filters import CommandStart, and_f
+import asyncio
+
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import CommandStart, and_f, StateFilter
+from aiogram.fsm.context import FSMContext
 from environs import Env
 
 from filters.admin_filters import IsAdmin
 from keyboards.inline_admin import admin_kb
+from states.admin_states import InputNews
 
 admin_router: Router = Router()
 
@@ -16,3 +20,22 @@ env.read_env()
 async def start_admin(message: Message):
     await message.answer('Выбери действие:',
                          reply_markup=admin_kb)
+
+
+@admin_router.callback_query(F.data == 'mailing_list_pressed')
+async def request_mailing(callback: CallbackQuery, state: FSMContext):
+    await callback.message.answer(text='Отправьте контент для рассылки:')
+    await state.set_state(InputNews.news)
+
+
+@admin_router.message(StateFilter(InputNews.news))
+async def input_news(message: Message, state: FSMContext):
+    await state.update_data(news=message.text)
+    count_user_ids_ = [707637895]
+    for count_user_id in count_user_ids_:
+        await message.copy_to(count_user_id)
+        await asyncio.sleep(.05)
+
+
+#382712245
+#519789698
