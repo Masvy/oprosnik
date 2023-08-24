cc5ed409f816099d749f53d3ba392339901adc54
Masvy <masvy.435@gmail.com>
2023-08-24 18:37:54 +0300

    docs: write docstrings

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -18,18 +18,29 @@ env.read_env()
 
 @admin_router.message(and_f(CommandStart(), IsAdmin(env('ADMIN_IDS'))))
 async def start_admin(message: Message):
+    '''Хэндлер срабатывает при вводе команды /start у админов.'''
     await message.answer('Выбери действие:',
                          reply_markup=admin_kb)
 
 
 @admin_router.callback_query(F.data == 'mailing_list_pressed')
 async def request_mailing(callback: CallbackQuery, state: FSMContext):
+    '''
+    Хэндлер для кнопки «Рассылка».
+
+    Переводит ввода контента.
+    '''
     await callback.message.answer(text='Отправьте контент для рассылки:')
     await state.set_state(InputNews.news)
 
 
 @admin_router.message(StateFilter(InputNews.news))
 async def input_news(message: Message, state: FSMContext):
+    '''
+    Хэндлер срабатывает на введение контента.
+
+    Начинает рассылку по пользователям.
+    '''
     await state.update_data(news=message.text)
     count_user_ids_ = [707637895]
     for count_user_id in count_user_ids_:
