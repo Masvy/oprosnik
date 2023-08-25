2119a8468056aa8899788c600e2bd314def4b914
Masvy <masvy.435@gmail.com>
2023-08-25 15:32:31 +0300

    chore: write a new lines of text

--- tgbot/lexicon/admin_lexicon.py
+++ tgbot/lexicon/admin_lexicon.py
@@ -1,3 +1,8 @@
 ADMIN_MENU: dict[str: str] = {
-    'mailing_list': 'Рассылка'
+    'mailing_list': 'Рассылка',
+    'start_survay': 'Пройти тестирование'
+}
+
+COMMANDS_LIST: dict[str: str] = {
+    '/start': 'Начать сначала'
 }
